0a845264e57a22b07d593a5dc454d1cca0cb26f8
guyguygvy <138956159+guyguygvy@users.noreply.github.com>
2025-03-10 18:18:33 -0700

    Some GameStates are mixed up??, will fix soon

--- VGP334/22_HelloPhysics/WinMain.cpp
+++ VGP334/22_HelloPhysics/WinMain.cpp
@@ -7,7 +7,7 @@ using namespace ENgines;
 int WINAPI WinMain(HINSTANCE instance, HINSTANCE, LPSTR, int)
 {
 	AppConfig config;
-	config.appName = L"Hello Sharacter Animation";
+	config.appName = L"Hello Physics";
 
 	App& myApp = MainApp();
 	myApp.AddState<GameState>("GameState");
